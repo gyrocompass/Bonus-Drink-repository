53bdfd8bafa37b37852095063ba0f8b9a8183050
gyrocompass <122189206+gyrocompass@users.noreply.github.com>
2023-01-28 14:03:47 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,15 +1,5 @@
 class BonusDrink
   def self.total_count_for(amount)
-    bottles = amount
-    while bottles >= 3
-      bonus = bottles / 3
-      amount += bonus
-      bottles = bottles % 3 + bonus
-    end
-
-    puts "トータルで飲める本数は#{amount}です。"
-
+    0
   end
-  
-return
 end
